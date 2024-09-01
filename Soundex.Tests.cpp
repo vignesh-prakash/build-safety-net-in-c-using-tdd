c52882fda20e1301d84567a089c4659fec41daad
vignesh-prakash <vignesh24p@gmail.com>
2024-09-01 18:01:10 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,23 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
-  ASSERT_STREQ(soundex,"A200");
+ 
+ generateSoundex("AX", soundex);
+ ASSERT_STREQ(soundex,"A200");
+
+ generateSoundex("soundex", soundex);
+ ASSERT_STREQ(soundex,"S532");
+
+ generateSoundex("Robbert", soundex);
+ ASSERT_STREQ(soundex,"R163");
+
+ generateSoundex("R", soundex);
+ ASSERT_STREQ(soundex,"R000");
+
+ generateSoundex("O'Hara", soundex);
+ ASSERT_STREQ(soundex,"O600");
+
+ generateSoundex("aou", soundex);
+ ASSERT_STREQ(soundex,"A000");
+ 
 }
